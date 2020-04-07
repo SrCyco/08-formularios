4a1807fc5697e256abdd45b5ea3a4cebda5d9920
Sergio Niño <sergioanino@qdata.io>
2020-04-07 12:42:26 -0500

    Add nested form gruop and validate this child controls

--- src/app/components/reactive/reactive.component.ts
+++ src/app/components/reactive/reactive.component.ts
@@ -18,16 +18,46 @@ export class ReactiveComponent implements OnInit {
   ngOnInit() {
   }
 
+  get validationName(): boolean {
+    return this.forma.get('nombre').invalid && this.forma.get('nombre').touched;
+  }
+  get validationLastname(): boolean {
+    return this.forma.get('apellido').invalid && this.forma.get('apellido').touched;
+  }
+  get validationMail(): boolean {
+    return this.forma.get('correo').invalid && this.forma.get('correo').touched;
+  }
+  get validationDistrito(): boolean {
+    return this.forma.get('direccion.distrito').invalid && this.forma.get('direccion.distrito').touched;
+  }
+  get validationCity(): boolean {
+    return this.forma.get('direccion.ciudad').invalid && this.forma.get('direccion.ciudad').touched;
+  }
+
   crearFormulario() {
     this.forma = this.fb.group({
       nombre: ['', [Validators.required, Validators.minLength(5)]],
       apellido: ['', Validators.required],
-      correo: ['', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')]]
+      correo: ['', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')]],
+      direccion: this.fb.group({
+        distrito: ['', Validators.required],
+        ciudad: ['', Validators.required]
+      })
     });
   }
 
   guardar() {
     console.log(this.forma);
+    if (this.forma.invalid) {
+      return Object.values(this.forma.controls).forEach( control => {
+        if (control instanceof FormGroup) {
+          Object.values(control.controls).forEach( controlChild => controlChild.markAsTouched());
+        } else {
+          control.markAsTouched();
+        }
+      });
+    }
+
   }
 
 }
